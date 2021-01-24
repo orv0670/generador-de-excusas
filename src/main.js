3d5dc3bb9ff9fd333e663f3da8f92e1aa895ff0e
orv0670 <orv0670@gmail.com>
2021-01-24 03:00:59 +0000

    actualizacion

--- src/main.js
+++ src/main.js
@@ -2,9 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var who = [
+    "my perro",
+    "my abuela",
+    "mi gato",
+    "naruto",
+    "mi novia",
+    "mi mama"
+  ];
+  var what = [
+    "me obligo a ir con el doctor",
+    "se comio mi tarrea",
+    "me ataco",
+    "me olbigo a hacer de comer",
+    "se enfermo",
+    "se enojo conmigo"
+  ];
+  var where = [
+    "en mi casa",
+    "en el cine",
+    "en el supermecado",
+    "en la playa",
+    "en un restaurante",
+    "en konoha"
+  ];
+
+  var sentencia =
+    who[rand(who.length)] +
+    " " +
+    what[rand(what.length)] +
+    " " +
+    where[rand(where.length)];
+
+  let excuseSpan = document.querySelector("#excuse");
+  excuseSpan.innerHTML = sentencia;
 };
+
+function rand(length) {
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
